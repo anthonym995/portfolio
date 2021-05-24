2fc5ab29018e1395aaba333154958a79c00bf358
Anthony <78245507+Anthony-M1995@users.noreply.github.com>
2021-05-24 11:21:04 +0530

    All files added

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+// Javascript document
+
+function openNav() {
+    document.getElementById("nav").style.width = "280px";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+}
+
+window.addEventListener('scroll',function() {
+    let header = document.querySelector('#header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-acitve',windowPosition);
+
+})
+window.addEventListener('scroll',function() {
+    let header = document.querySelector('.close');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('close-top',windowPosition);
+
+})
